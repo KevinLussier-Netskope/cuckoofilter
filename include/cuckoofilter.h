a5524750e220df92e0d30f0b8b34ebd8ce13273f
Kevin Lussier <klussier@Kevins-MacBook-Pro.local>
2020-08-11 16:22:11 -0700

    Add ability to save active filter to a file. Add ability to load a saved a filter from a file or block of memory.

--- include/cuckoofilter.h
+++ include/cuckoofilter.h
@@ -3,7 +3,7 @@
 
 #include <assert.h>
 #include <algorithm>
-
+#include <fstream>
 #include "singletable.h"
 #include "twoindependentmultiplyshift.h"
 
@@ -45,6 +45,12 @@ class BaseCuckooFilter
 
   // size of the filter in bytes.
   virtual size_t SizeInBytes() const = 0;
+
+  // save the filter to a file
+  virtual bool Save(const std::string path) const = 0;
+
+  // returns if the filter if valid
+  virtual bool Valid() const = 0;
 };
 
 // A cuckoo filter class exposes a Bloomier filter interface,
@@ -70,9 +76,20 @@ class CuckooFilter : public BaseCuckooFilter<ItemType> {
     bool used;
   } VictimCache;
 
+  // The header we will use when we save the filter
+  struct save_header {
+    size_t num_buckets_;
+    size_t num_items_;
+    VictimCache victim_;
+    HashFamily hasher_;
+  };
+
   VictimCache victim_;
 
   HashFamily hasher_;
+
+  // Buffer created if we read the filter from a file
+  char *readbuf_;
 
   inline size_t IndexHash(uint32_t hv) const {
     // table_->num_buckets is always a power of two, so modulo can be replaced
@@ -111,7 +128,8 @@ class CuckooFilter : public BaseCuckooFilter<ItemType> {
   double BitsPerItem() const { return 8.0 * table_->SizeInBytes() / Size(); }
 
  public:
-  explicit CuckooFilter(const size_t max_num_keys) : num_items_(0), victim_(), hasher_() {
+  explicit CuckooFilter(const size_t max_num_keys) : table_(nullptr), num_items_(0), victim_(), hasher_(), readbuf_(nullptr) {
+    // Build the filter fased on the max number of keys and the bit size.
     size_t assoc = 4;
     size_t num_buckets = upperpower2(std::max<uint64_t>(1, max_num_keys / assoc));
     double frac = (double)max_num_keys / num_buckets / assoc;
@@ -119,10 +137,61 @@ class CuckooFilter : public BaseCuckooFilter<ItemType> {
       num_buckets <<= 1;
     }
     victim_.used = false;
-    table_ = new TableType<bits_per_item>(num_buckets);
+    try {
+      table_ = new(std::nothrow) TableType<bits_per_item>(num_buckets);
+    } catch (std::bad_alloc& ba) {
+      // Caller should call Valid() to ensure filter is built
+    }
   }
 
-  ~CuckooFilter() { delete table_; }
+  explicit CuckooFilter(void *addr, size_t length) : table_(nullptr), num_items_(0), victim_(), hasher_(), readbuf_(nullptr) {
+    // Load the filter from the specified buffer. We will not own the data we
+    // read in, so the caller better not free it...
+    struct save_header *sh = reinterpret_cast<struct save_header *>(addr);
+    num_items_ = sh->num_items_;
+    victim_ = sh->victim_;
+    hasher_ = sh->hasher_;
+    char *data = (char *)addr + sizeof(struct save_header);
+    length = length - sizeof(struct save_header);
+    try {
+      table_ = new TableType<bits_per_item>(data, length);
+    } catch (std::bad_alloc& ba) {
+      // Caller should call Valid() to ensure filter is built
+    }
+  }
+
+  explicit CuckooFilter(const std::string &path) : table_(nullptr), num_items_(0), victim_(), hasher_(), readbuf_(nullptr) {
+    // Read the saved filter from the specified path. We will own the data we
+    // read in and free it in the destructor.
+    std::ifstream rf(path, std::ios::in | std::ios::binary | std::ios::ate);
+    if (!rf) {
+      return;
+    }
+    size_t size = rf.tellg();
+    readbuf_ = new char[size];
+    if (!readbuf_) {
+      return;
+    }
+    rf.seekg(0);
+    if (!rf.read(readbuf_, size)) {
+      return;
+    }
+    rf.close();
+
+    struct save_header *sh = reinterpret_cast<struct save_header *>(readbuf_);
+    num_items_ = sh->num_items_;
+    victim_ = sh->victim_;
+    hasher_ = sh->hasher_;
+    char *data = readbuf_ + sizeof(struct save_header);
+    size_t length = size - sizeof(struct save_header);
+    try {
+      table_ = new TableType<bits_per_item>(data, length);
+    } catch (std::bad_alloc& ba) {
+      // Caller should call Valid() to ensure filter is built
+    }
+  }
+
+  ~CuckooFilter() { delete table_; delete readbuf_; }
 
   // Add an item to the filter.
   Status Add(const ItemType &item);
@@ -142,6 +211,37 @@ class CuckooFilter : public BaseCuckooFilter<ItemType> {
 
   // size of the filter in bytes.
   size_t SizeInBytes() const { return table_->SizeInBytes(); }
+
+  // save the filter to a file
+  bool Save(const std::string path) const {
+    // Build the header
+    struct save_header sh;
+    sh.num_buckets_ = table_->NumBuckets();
+    sh.num_items_ = Size();
+    sh.victim_ = victim_;
+    sh.hasher_ = hasher_;
+
+    const unsigned char *data = table_->Data();
+    size_t length = table_->SizeInBytes();
+    std::ofstream wf(path, std::ios::out | std::ios::binary);
+    if(!wf) {
+      return false;
+    }
+    wf.write(reinterpret_cast<const char*>(&sh), sizeof(sh));
+    wf.write(reinterpret_cast<const char*>(data), length);
+    wf.close();
+    if(!wf.good()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool Valid() const {
+    // Valid means we have a table loaded
+    return table_ != nullptr;
+  }
+
 };
 
 template <typename ItemType, size_t bits_per_item,
@@ -180,9 +280,6 @@ Status CuckooFilter<ItemType, bits_per_item, TableType, HashFamily>::AddImpl(
     curindex = AltIndex(curindex, curtag);
   }
 
-  victim_.index = curindex;
-  victim_.tag = curtag;
-  victim_.used = true;
   return Ok;
 }
 
@@ -251,7 +348,7 @@ std::string CuckooFilter<ItemType, bits_per_item, TableType, HashFamily>::Info()
      << "\t\t" << table_->Info() << "\n"
      << "\t\tKeys stored: " << Size() << "\n"
      << "\t\tLoad factor: " << LoadFactor() << "\n"
-     << "\t\tHashtable size: " << (table_->SizeInBytes() >> 10) << " KB\n";
+     << "\t\tHashtable size: " << (table_->SizeInBytes()) << " bytes\n";
   if (Size() > 0) {
     ss << "\t\tbit/key:   " << BitsPerItem() << "\n";
   } else {
