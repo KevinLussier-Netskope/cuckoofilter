1213437bf2217ec7b7cd5d148b03f53a0b3709ec
Kevin Lussier <klussier@Kevins-MacBook-Pro.local>
2020-08-11 16:16:30 -0700

    Update test program to build filter of any size based on parameters. Update test program to build filter from saved file Update test program to build filter from memory mapped file.

--- example/test.cc
+++ example/test.cc
@@ -7,8 +7,12 @@
 #include <iomanip>
 #include <vector>
 #include <chrono>
+#include <fstream>
 
-#include <sys/resource.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 using cuckoofilter::CuckooFilter;
 using cuckoofilter::BaseCuckooFilter;
@@ -21,6 +25,54 @@ void usage()
   printf("        fp_mult is the number of false positives to test for as a multiple of item_count (e.g. 3)\n");
   exit(-1);
 }
+
+bool run_adds(BaseCuckooFilter<size_t> *filter, size_t total_items)
+{
+  auto start = std::chrono::high_resolution_clock::now();
+
+  // Insert items to this filter
+  size_t num_inserted = 0;
+  for (size_t i = 0; i < total_items; i++, num_inserted++) {
+    if (filter->Add(i) != cuckoofilter::Ok) {
+      std::cout << "failed to insert item " << i << "\n";
+      return false;
+    }
+  }
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+  std::cout << num_inserted << " entries added in " << duration.count() << " microseconds " << std::endl;
+  return true;
+}
+
+bool run_contains(BaseCuckooFilter<size_t> *filter, size_t total_items, size_t fp_mult)
+{
+  // Check if previously inserted items are in the filter, expected
+  // true for all items
+  for (size_t i = 0; i < total_items; i++) {
+    if (filter->Contain(i) != cuckoofilter::Ok) {
+      std::cout << "False negative seen at index " << i << std::endl;
+      return false;
+    }
+  }
+
+  // Check non-existing items, a few false positives expected
+  size_t total_queries = 0;
+  size_t false_queries = 0;
+  for (size_t i = total_items; i < (fp_mult + 1) * total_items; i++) {
+    if (filter->Contain(i) == cuckoofilter::Ok) {
+      false_queries++;
+    }
+    total_queries++;
+  }
+
+  // Output the measured false positive rate
+  std::cout.precision(15);
+  std::cout << "false queries: " << false_queries << ", total_queries: " << total_queries << "\n";
+  std::cout << std::fixed << "false positive rate is "
+            << 100.0 * false_queries / total_queries << "%\n";
+  return true;
+}
+
 
 int main(int argc, const char **argv)
 {
@@ -49,12 +101,6 @@ int main(int argc, const char **argv)
     bits_per_item << " bits per item and a false positive multiple of " <<
     fp_mult << "\n";
 
-  struct rusage startuse;
-  memset(&startuse, 0, sizeof(startuse));
-  if (getrusage(RUSAGE_SELF, &startuse) < 0) {
-    std::cout << "failed to get usage before adds: << " << errno;
-  }
-
   // Create a cuckoo filter where each item is of type size_t and
   // use 12 bits for each item:
   //    CuckooFilter<size_t, 12> filter(total_items);
@@ -62,6 +108,10 @@ int main(int argc, const char **argv)
   // PackedTable, accepting keys of size_t type and making 13 bits
   // for each key:
   //   CuckooFilter<size_t, 13, cuckoofilter::PackedTable> filter(total_items);
+
+  /*
+   * Run the normal test.
+   */
   BaseCuckooFilter<size_t> *filter;
   switch(bits_per_item) {
     case 2: filter = new CuckooFilter<size_t, 2>(total_items); break;
@@ -71,65 +121,119 @@ int main(int argc, const char **argv)
     case 16: filter = new CuckooFilter<size_t, 16>(total_items); break;
     case 32: filter = new CuckooFilter<size_t, 32>(total_items); break;
   }
-
-  struct rusage createuse;
-  memset(&createuse, 0, sizeof(createuse));
-  if (getrusage(RUSAGE_SELF, &createuse) < 0) {
-    std::cout << "failed to get usage after create: << " << errno;
-  }
-  std::cout << "memory usage after create: " << createuse.ru_maxrss << "\n";
-  std::cout << "memory used by create: " << createuse.ru_maxrss - startuse.ru_maxrss << "\n";
-  std::cout << "size reported by object: " << filter->SizeInBytes() << "\n";
-
-  auto start = std::chrono::high_resolution_clock::now();
-  // Insert items to this cuckoo filter
-  size_t num_inserted = 0;
-  for (size_t i = 0; i < total_items; i++, num_inserted++) {
-    if (filter->Add(i) != cuckoofilter::Ok) {
-      std::cout << "failed to insert item " << i << "\n";
-      break;
-    }
-  }
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
-  std::cout << num_inserted << " entries added in " << duration.count() << " microseconds " << std::endl;
-
-  struct rusage adduse;
-  memset(&adduse, 0, sizeof(adduse));
-  if (getrusage(RUSAGE_SELF, &adduse) < 0) {
-    std::cout << "failed to get usage after adds: << " << errno;
-  }
-  std::cout << "memory usage after adds: " << adduse.ru_maxrss << "\n";
-  std::cout << "memory used by adds: " << adduse.ru_maxrss - createuse.ru_maxrss << "\n";
-
-  // Check if previously inserted items are in the filter, expected
-  // true for all items
-  for (size_t i = 0; i < num_inserted; i++) {
-    assert(filter->Contain(i) == cuckoofilter::Ok);
+  if (!filter->Valid()) {
+    std::cout << "Failed to create cuckoo filter with <size_t, " << bits_per_item << "> and " << total_items << " items\n";
+    return 1;
   }
 
-  // Check non-existing items, a few false positives expected
-  size_t total_queries = 0;
-  size_t false_queries = 0;
-  for (size_t i = total_items; i < (fp_mult + 1) * total_items; i++) {
-    if (filter->Contain(i) == cuckoofilter::Ok) {
-      false_queries++;
-    }
-    total_queries++;
+  // Run the add test
+  if (!run_adds(filter, total_items)) {
+    std::cout << "Add test failed\n";
+    return 1;
   }
 
-  // Output the measured false positive rate
-  std::cout.precision(15);
-  std::cout << "false queries: " << false_queries << ", total_queries: " << total_queries << "\n";
-  std::cout << std::fixed << "false positive rate is "
-            << 100.0 * false_queries / total_queries << "%\n";
+  // Dump the filter info (after the add test so the added items are included)
+  std::cout << filter->Info();
 
-  struct rusage enduse;
-  memset(&enduse, 0, sizeof(enduse));
-  if (getrusage(RUSAGE_SELF, &enduse) < 0) {
-    std::cout << "failed to get usage at end: << " << errno;
+  // Run the contain test
+  if (!run_contains(filter, total_items, fp_mult)) {
+    std::cout << "Contain test failed\n";
+    return 1;
   }
-  std::cout << "memory usage at end: " << enduse.ru_maxrss << "\n";
+
+  // Save the filter to disk for the next test
+  std::string filename = "filter.dat";
+  filter->Save(filename);
+
+  // Delete the filter to free up memory
+  delete filter;
+
+  /*
+   * Run the filename test.
+   */
+  // Create a new filter that will load in the saved filter
+  switch(bits_per_item) {
+    case 2: filter = new CuckooFilter<size_t, 2>(filename); break;
+    case 4: filter = new CuckooFilter<size_t, 4>(filename); break;
+    case 8: filter = new CuckooFilter<size_t, 8>(filename); break;
+    case 12: filter = new CuckooFilter<size_t, 12>(filename); break;
+    case 16: filter = new CuckooFilter<size_t, 16>(filename); break;
+    case 32: filter = new CuckooFilter<size_t, 32>(filename); break;
+  }
+  if (!filter->Valid()) {
+    std::cout << "Failed to create cuckoo filter with <size_t, " << bits_per_item << "> and " << filename << " items\n";
+    return 1;
+  }
+
+  // Dump the filter info (it should be the same as the original filter)
+  std::cout << filter->Info();
+
+  // Run the contain test
+  if (!run_contains(filter, total_items, fp_mult)) {
+    std::cout << "Contain test failed\n";
+    return 1;
+  }
+
+  // Delete the filter to free up memory
+  delete filter;
+
+  /*
+   * Run the mmap test.
+   */
+  // Open the saved file as a memory mapped file
+  int fd = open(filename.c_str(), O_RDONLY, 0644);
+  if (fd < 0) {
+    std::cout << "Failed to open " << filename << " for memory mapping\n";
+    return 1;
+  }
+
+  // Get the size of the file
+  struct stat s;
+  if (fstat(fd, &s) < 0) {
+    std::cout << "Failed to stat " << filename << std::endl;
+    return 1;
+  }
+
+  // Memory map the file
+  void *data = mmap(NULL, s.st_size, PROT_READ, MAP_SHARED, fd, 0);
+  if (data == MAP_FAILED) {
+    std::cout << "Failed to memory map " << filename << ", " << strerror(errno) << std::endl;
+    close(fd);
+    return 1;
+  }
+  if (data == NULL) {
+    std::cout << "data is NULL\n";
+  }
+  madvise(data, s.st_size, MADV_WILLNEED);
+
+  // Create a new filter that will load in the memory mapped filter
+  switch(bits_per_item) {
+    case 2: filter = new CuckooFilter<size_t, 2>(data, s.st_size); break;
+    case 4: filter = new CuckooFilter<size_t, 4>(data, s.st_size); break;
+    case 8: filter = new CuckooFilter<size_t, 8>(data, s.st_size); break;
+    case 12: filter = new CuckooFilter<size_t, 12>(data, s.st_size); break;
+    case 16: filter = new CuckooFilter<size_t, 16>(data, s.st_size); break;
+    case 32: filter = new CuckooFilter<size_t, 32>(data, s.st_size); break;
+  }
+  if (!filter->Valid()) {
+    std::cout << "Failed to create cuckoo filter with <size_t, " << bits_per_item << "> and memory mapped " << filename << " items\n";
+    return 1;
+  }
+
+  // Dump the filter info (it should be the same as the original filter)
+  std::cout << filter->Info();
+
+  // Run the contain test
+  if (!run_contains(filter, total_items, fp_mult)) {
+    std::cout << "Contain test failed\n";
+    return 1;
+  }
+
+  // Delete the filter to free up memory
+  delete filter;
+
+  // Close the open file
+  close(fd);
 
   return 0;
 }
